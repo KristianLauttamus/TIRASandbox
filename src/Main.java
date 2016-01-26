60abee6633dabbb0eae1baae046dfa4c97c33f4a
Kristian Lauttamus <kristian.lauttamus@gmail.com>
2016-01-26 16:03:21 +0200

    Week 2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Week2 week2 = new Week2();
+        
+        week2.tehtava2();
+    }
+}
--- src/Week2.java
+++ src/Week2.java
@@ -1,9 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 public class Week2 {
-
+    public void tehtava2(){
+        tehtava2Part2(3);
+    }
+    
+    public void tehtava2Part1(int maara){
+        for(int i = 0; i < maara; i++){
+            System.out.print('*');
+        }
+    }
+    
+    public void tehtava2Part2(int rivit){
+        for(int i = rivit; 0 < i; i--){
+            this.tehtava2Part1(i);
+            System.out.println("");
+        }
+    }
 }
